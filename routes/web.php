b7424c8ff01c65bdf13acfe6bd0aafa08c710a06
Andrei Susanu <andrei.susanu@gmail.com>
2020-07-02 13:32:25 +0300

    Cleanup and small customizations

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/home');
 
 Auth::routes();
 
