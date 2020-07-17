ed2969cda7b8cc19e86b2ab44e3029a2faea56b0
Andrei Susanu <andrei.susanu@gmail.com>
2020-07-17 09:26:50 +0300

    #19 - Request help form - WIP

---
+++ resources/lang/ro/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Aceste credentiale nu se potrivesc cu înregistrările noastre.',
+    'throttle' => 'Prea multe încercări de conectare. Încercați din nou în :seconds secunde.',
+
+];
