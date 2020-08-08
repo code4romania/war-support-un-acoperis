aea6f648018dbe5e519eb6268c55e728eb66b3bc
wasile <wasile@gmail.com>
2020-08-08 15:40:00 +0300

    #47 extracted js classes from template

--- resources/views/admin/clinic-list.blade.php
+++ resources/views/admin/clinic-list.blade.php
@@ -151,8 +151,9 @@
 @endsection
 
 @section('scripts')
-    <script src="https://cdn.jsdelivr.net/npm/choices.js/public/assets/scripts/choices.min.js"></script>
-    <script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/choices.js/public/assets/scripts/choices.min.js"></script>
+    <script type="text/javascript" src="{{ mix('js/clinics-renderer.min.js') }}"></script>
+    <script type="text/javascript">
         $(document).ready(function () {
             let pageState = {};
             pageState.page = 1;
@@ -169,7 +170,7 @@
             $('.resultsPerPage').val(pageState.perPage);
 
             let deleteClinicId = null;
-            let render = new ClinicsRenderer();
+            let render = new ClinicsRenderer('{{ route('ajax.clinic-list') }}', '{{ __('Edit') }}', '{{ __('Delete') }}', '{{ __('See details') }}');
             render.renderHelpRequests(pageState);
 
             $('#searchFilter').on('keyup', e => {
@@ -205,25 +206,19 @@
             $( "#categoryFilter" ).change(function() {
                 const selectedCategories = $(this).children("option:selected").map(function(){ return this.value }).get().join("|");
                 pageState.categories = selectedCategories;
-                if (selectedCategories.length) {
-                    $.SetQueryStringParameter('categories', pageState.categories);
-                }
+                $.SetQueryStringParameter('categories', pageState.categories);
                 render.renderHelpRequests(pageState);
             });
 
             $( "#countryFilter" ).change(function() {
                 pageState.country = $(this).val();
-                if (pageState.country.length) {
-                    $.SetQueryStringParameter('country', pageState.country);
-                }
+                $.SetQueryStringParameter('country', pageState.country);
                 render.renderHelpRequests(pageState);
             });
 
             $( "#cityFilter" ).change(function() {
                 pageState.city = $(this).val();
-                if (pageState.city.length) {
-                    $.SetQueryStringParameter('city', pageState.city);
-                }
+                $.SetQueryStringParameter('city', pageState.city);
                 render.renderHelpRequests(pageState);
             });
 
@@ -263,105 +258,5 @@
                 timer = setTimeout(callback, ms);
             };
         })();
-
-        class ClinicsRenderer {
-            renderHelpRequests(pageState) {
-                axios.get('{{ route('ajax.clinic-list') }}', {params: pageState})
-                    .then(res => {
-                        this.updateResultsCount(res.data.total);
-                        this.renderTable(res.data.data);
-                        this.renderPagination(res.data);
-                    });
-            }
-
-            emptyTable() {
-                $('#tableBody tr').remove();
-            }
-
-            updateResultsCount(count) {
-                $('#totalResults').text(count);
-
-                if (0 === count) {
-                    $('.no-results').removeClass('d-none').addClass('d-flex');
-                    $('.details').addClass('d-none');
-                } else {
-                    $('.no-results').removeClass('d-flex').addClass('d-none');
-                    $('.details').removeClass('d-none');
-                }
-            }
-
-            renderTable(responseData) {
-                this.emptyTable();
-
-                $.each(responseData, function(key, value) {
-                    let row = '<tr id="clinic-container-' + value.id + '">\n' +
-                        '    <td><a href="/admin/clinic/' + value.id + '">' + value.name + '</a></td>\n' +
-                        '    <td>' + value.country + '</td>\n' +
-                        '    <td>' + value.city + '</td>\n' +
-                        '    <td class="text-right">\n' +
-                        '        <a href="/admin/clinic/edit/' + value.id + '" class="btn btn-secondary btn-icon btn-sm" data-original-title="{{ __('Edit') }}" title="{{ __('Edit') }}">\n' +
-                        '            {{ __('Edit') }}\n' +
-                        '        </a>\n' +
-                        '        <a href="#" class="btn btn-sm btn-danger mb-2 mb-sm-0 delete-clinic" data-id=' + value.id + '>{{ __('Delete') }}</a>\n' +
-                        '        <a href="/admin/clinic/' + value.id + '" class="btn btn-sm btn-info mb-2 mb-sm-0">{{ __('See details') }}</a>\n' +
-                        '    </td>\n' +
-                        '</tr>';
-
-                    $('#tableBody').append(row);
-                });
-            }
-
-            renderPagination(response) {
-                $('.pagination li').remove();
-
-                if (1 === response.last_page) {
-                    return;
-                }
-
-                let morePages = '<li class="page-item disabled"><a class="page-link" href="#">...</a></li>';
-
-                let currentPage = '<li class="page-item active"><a class="page-link" data-page="' + response.current_page + '" href="#">' + response.current_page + ' <span class="sr-only">(current)</span></a></li>';
-
-                let firstPage = '';
-                if (response.current_page > 1) {
-                    firstPage = '<li class="page-item"><a class="page-link" data-page="1" href="#">1</a></li>';
-                }
-
-                let step = response.current_page
-                let counter = 0;
-
-                let previousPages = '';
-                while(step > 2 && 2 > counter) {
-                    counter++;
-                    step--;
-                    previousPages = '<li class="page-item"><a class="page-link" data-page="' + step + '" href="#">' + step + '</a></li>' + previousPages;
-                }
-
-                if (response.current_page > 4) {
-                    previousPages = morePages + previousPages;
-                }
-
-                step = response.current_page;
-                counter = 0;
-
-                let nextPages = '';
-                while(step < response.last_page - 1 && 2 > counter) {
-                    counter++;
-                    step++;
-                    nextPages += '<li class="page-item"><a class="page-link" data-page="' + step + '" href="#">' + step + '</a></li>';
-                }
-
-                if ((response.last_page - response.current_page) > 3) {
-                    nextPages += morePages;
-                }
-
-                let lastPage = '';
-                if (response.current_page < response.last_page) {
-                    lastPage = '<li class="page-item"><a class="page-link" data-page="' + response.last_page + '" href="#">' + response.last_page + '</a></li>';
-                }
-
-                $('.pagination').append(firstPage).append(previousPages).append(currentPage).append(nextPages).append(lastPage);
-            }
-        }
     </script>
 @endsection
--- resources/views/frontend/clinic-list.blade.php
+++ resources/views/frontend/clinic-list.blade.php
@@ -210,6 +210,7 @@
 
 @section('scripts')
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/choices.js/public/assets/scripts/choices.min.js"></script>
+    <script type="text/javascript" src="{{ mix('js/clinics-front-renderer.min.js') }}"></script>
     <script type="text/javascript">
         $(document).ready(function () {
             let pageState = {};
@@ -226,8 +227,7 @@
 
             $('.resultsPerPage').val(pageState.perPage);
 
-            let deleteClinicId = null;
-            let render = new ClinicsRenderer();
+            let render = new ClinicsFrontRenderer('{{ route('ajax.clinic-list') }}', '{{ __('See details') }}', '{{ $locale }}');
             render.renderHelpRequests(pageState);
 
             $('.resultsPerPage').on('change', function () {
@@ -259,25 +259,19 @@
             $( "#categoryFilter" ).change(function() {
                 const selectedCategories = $(this).children("option:selected").map(function(){ return this.value }).get().join("|");
                 pageState.categories = selectedCategories;
-                if (selectedCategories.length) {
-                    $.SetQueryStringParameter('categories', pageState.categories);
-                }
+                $.SetQueryStringParameter('categories', pageState.categories);
                 render.renderHelpRequests(pageState);
             });
 
             $( "#countryFilter" ).change(function() {
                 pageState.country = $(this).val();
-                if (pageState.country.length) {
-                    $.SetQueryStringParameter('country', pageState.country);
-                }
+                $.SetQueryStringParameter('country', pageState.country);
                 render.renderHelpRequests(pageState);
             });
 
             $( "#cityFilter" ).change(function() {
                 pageState.city = $(this).val();
-                if (pageState.city.length) {
-                    $.SetQueryStringParameter('city', pageState.city);
-                }
+                $.SetQueryStringParameter('city', pageState.city);
                 render.renderHelpRequests(pageState);
             });
         });
@@ -289,101 +283,5 @@
                 timer = setTimeout(callback, ms);
             };
         })();
-
-        class ClinicsRenderer {
-            renderHelpRequests(pageState) {
-                axios.get('{{ route('ajax.clinic-list') }}', {params: pageState})
-                    .then(res => {
-                        this.updateResultsCount(res.data.total);
-                        this.renderTable(res.data.data);
-                        this.renderPagination(res.data);
-                    });
-            }
-
-            emptyTable() {
-                $('#tableBody tr').remove();
-            }
-
-            updateResultsCount(count) {
-                $('#totalResults').text(count);
-
-                if (0 === count) {
-                    $('.no-results').removeClass('d-none').addClass('d-flex');
-                    $('.details').addClass('d-none');
-                } else {
-                    $('.no-results').removeClass('d-flex').addClass('d-none');
-                    $('.details').removeClass('d-none');
-                }
-            }
-
-            renderTable(responseData) {
-                this.emptyTable();
-
-                $.each(responseData, function(key, value) {
-                    let row = '<tr id="clinic-container-' + value.id + '">\n' +
-                        '    <td><a href="/{{ $locale }}/clinic/' + value.slug + '">' + value.name + '</a></td>\n' +
-                        '    <td>' + value.country + '</td>\n' +
-                        '    <td>' + value.city + '</td>\n' +
-                        '    <td class="text-right">\n' +
-                        '        <a href="/{{ $locale }}/clinic/' + value.slug + '" class="btn btn-sm btn-info mb-2 mb-sm-0">{{ __('See details') }}</a>\n' +
-                        '    </td>\n' +
-                        '</tr>';
-
-                    $('#tableBody').append(row);
-                });
-            }
-
-            renderPagination(response) {
-                $('.pagination li').remove();
-
-                if (1 === response.last_page) {
-                    return;
-                }
-
-                let morePages = '<li class="page-item disabled"><a class="page-link" href="#">...</a></li>';
-
-                let currentPage = '<li class="page-item active"><a class="page-link" data-page="' + response.current_page + '" href="#">' + response.current_page + ' <span class="sr-only">(current)</span></a></li>';
-
-                let firstPage = '';
-                if (response.current_page > 1) {
-                    firstPage = '<li class="page-item"><a class="page-link" data-page="1" href="#">1</a></li>';
-                }
-
-                let step = response.current_page
-                let counter = 0;
-
-                let previousPages = '';
-                while(step > 2 && 2 > counter) {
-                    counter++;
-                    step--;
-                    previousPages = '<li class="page-item"><a class="page-link" data-page="' + step + '" href="#">' + step + '</a></li>' + previousPages;
-                }
-
-                if (response.current_page > 4) {
-                    previousPages = morePages + previousPages;
-                }
-
-                step = response.current_page;
-                counter = 0;
-
-                let nextPages = '';
-                while(step < response.last_page - 1 && 2 > counter) {
-                    counter++;
-                    step++;
-                    nextPages += '<li class="page-item"><a class="page-link" data-page="' + step + '" href="#">' + step + '</a></li>';
-                }
-
-                if ((response.last_page - response.current_page) > 3) {
-                    nextPages += morePages;
-                }
-
-                let lastPage = '';
-                if (response.current_page < response.last_page) {
-                    lastPage = '<li class="page-item"><a class="page-link" data-page="' + response.last_page + '" href="#">' + response.last_page + '</a></li>';
-                }
-
-                $('.pagination').append(firstPage).append(previousPages).append(currentPage).append(nextPages).append(lastPage);
-            }
-        }
     </script>
 @endsection
