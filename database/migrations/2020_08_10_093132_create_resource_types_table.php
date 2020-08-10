f9cd3e8dbb9dba9cd2c46095171905c49ccbd003
wasile <wasile@gmail.com>
2020-08-10 12:44:25 +0300

    #81 created resources database structure

---
+++ database/migrations/2020_08_10_093132_create_resource_types_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateResourceTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('resource_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 64);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('resource_types');
+    }
+}
