4705a80daa5717867d0e3172965030d95b841483
Andrei Susanu <andrei.susanu@gmail.com>
2020-08-12 14:11:32 +0300

    Adding Help Controller for EB health check

--- .platform/hooks/postdeploy/04_php_artisan_scout.sh
+++ .platform/hooks/postdeploy/04_php_artisan_scout.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo docker exec $(sudo docker ps -q) scout:import App\\Clinic
-sudo docker exec $(sudo docker ps -q) scout:import App\\HelpRequest
+sudo docker exec $(sudo docker ps -q) php artisan scout:import App\\Clinic
+sudo docker exec $(sudo docker ps -q) php artisan scout:import App\\HelpRequest
