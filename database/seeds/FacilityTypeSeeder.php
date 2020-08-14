4880f324f6d1ef63382960658c778a9e3d205f72
Andrei Susanu <andrei.susanu@gmail.com>
2020-08-14 07:52:49 +0300

    Host: Accommodation implementation (CRUD) - WIP

--- database/seeds/FacilityTypeSeeder.php
+++ database/seeds/FacilityTypeSeeder.php
@@ -13,21 +13,21 @@ class FacilityTypeSeeder extends Seeder
     public function run()
     {
         if (empty(FacilityType::all()->count())) {
-            FacilityType::create(['id' => 1, 'name' => 'Dotari esentiale (prosoape, lenjerie de pat, sapun, hartie igienica, perne)']);
-            FacilityType::create(['id' => 2, 'name' => 'Wi-fi']);
-            FacilityType::create(['id' => 3, 'name' => 'Televizor']);
-            FacilityType::create(['id' => 4, 'name' => 'Incalzire']);
-            FacilityType::create(['id' => 5, 'name' => 'Aer conditionat']);
-            FacilityType::create(['id' => 6, 'name' => 'Dulapuri/Sertare']);
+            FacilityType::create(['id' => 1, 'name' => 'Essential needs (towels, bad sheets, soap, toilet paper, pillows)']);
+            FacilityType::create(['id' => 2, 'name' => 'Wi-Fi']);
+            FacilityType::create(['id' => 3, 'name' => 'TV']);
+            FacilityType::create(['id' => 4, 'name' => 'Heating']);
+            FacilityType::create(['id' => 5, 'name' => 'Air conditioning']);
+            FacilityType::create(['id' => 6, 'name' => 'Cabinets / drawers']);
 
-            FacilityType::create(['id' => 7, 'name' => 'Detector de fum']);
-            FacilityType::create(['id' => 8, 'name' => 'Detector de gaze']);
-            FacilityType::create(['id' => 9, 'name' => 'Trusa de prim ajutor']);
-            FacilityType::create(['id' => 10, 'name' => 'Extinctor']);
-            FacilityType::create(['id' => 11, 'name' => 'Incuietoare la usa dormitorului']);
-            FacilityType::create(['id' => 12, 'name' => 'Lift pentru persoane cu dizabilitati']);
+            FacilityType::create(['id' => 7, 'name' => 'Smoke detector']);
+            FacilityType::create(['id' => 8, 'name' => 'Gas detector']);
+            FacilityType::create(['id' => 9, 'name' => 'First aid kit']);
+            FacilityType::create(['id' => 10, 'name' => 'Fire extinguisher']);
+            FacilityType::create(['id' => 11, 'name' => 'Lock on the bedroom door']);
+            FacilityType::create(['id' => 12, 'name' => 'Elevator for people with disabilities']);
 
-            FacilityType::create(['id' => 13, 'name' => 'other']);
+            FacilityType::create(['id' => 13, 'name' => 'Others needs']);
         }
     }
 }
