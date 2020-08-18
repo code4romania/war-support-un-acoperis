0f6b88a775b8d268b2b4cb52bce2f32d09844fcd
Andrei Susanu <andrei.susanu@gmail.com>
2020-08-18 09:13:51 +0300

    Naming route

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::redirect('/', '/ro');
-Route::get('/health', 'HealthController@check');
+Route::get('/health', 'HealthController@check')->name('health.check');
 
 /**
  * Administration routes
