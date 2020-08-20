25bc7165f0b7d3b3cf1369468772126e3b6bd0fb
wasile <wasile@gmail.com>
2020-08-20 13:18:10 +0300

    added ResourceTypeSeeder into DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -23,5 +23,6 @@ class DatabaseSeeder extends Seeder
          $this->call(HelpTypesSeeder::class);
          $this->call(AccomodationTypeSeeder::class);
          $this->call(FacilityTypeSeeder::class);
+         $this->call(ResourceTypeSeeder::class);
     }
 }
