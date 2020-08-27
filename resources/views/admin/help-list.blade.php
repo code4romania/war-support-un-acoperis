92ed59e35d16aa72795fefb7fde857e790ee23ae
Andrei Susanu <andrei.susanu@gmail.com>
2020-08-27 08:09:12 +0300

    #129 - Refactor Table Data Renderer - WIP

--- resources/views/admin/help-list.blade.php
+++ resources/views/admin/help-list.blade.php
@@ -128,7 +128,32 @@
 @endsection
 
 @section('scripts')
+    <script type="text/javascript" src="{{ mix('js/table-data-renderer.js') }}"></script>
     <script>
+        class HelpRequestRenderer extends TableDataRenderer {
+            renderTable(responseData) {
+                this.emptyTable();
+
+                $.each(responseData, function(key, value) {
+                    let row = '<tr>\n' +
+                        '    <td><a href="/admin/help/' + value.id + '">#' + value.id + '</a></td>\n' +
+                        '    <td>' + value.patient_full_name + '</td>\n' +
+                        '    <td>' + value.caretaker_full_name + '</td>\n' +
+                        '    <td>' + value.diagnostic + '</td>\n' +
+                        '    <td>' + $.TranslateRequestStatus(value.status) + '</td>\n' +
+                        '    <td>' + moment(value.created_at).locale('ro').format('LLL') + '</td>\n' +
+                        '    <td class="text-right">\n' +
+                        '        <a href="/admin/help/' + value.id + '" class="btn btn-info btn-icon btn-sm" data-original-title="{{ __('Details') }}" title="{{ __('Details') }}">\n' +
+                        '            {{ __('See details') }}\n' +
+                        '        </a>\n' +
+                        '    </td>\n' +
+                        '</tr>';
+
+                    $('#tableBody').append(row);
+                });
+            }
+        }
+
         $(document).ready(function () {
             let pageState = {};
             pageState.page = 1;
@@ -163,8 +188,8 @@
                 $('#endDateFilter').val(pageState.endDate);
             }
 
-            let render = new HelpRequestRenderer();
-            render.renderHelpRequests(pageState);
+            let render = new HelpRequestRenderer('{{ route('ajax.help-requests') }}');
+            render.renderData(pageState);
 
             $('#searchFilter').on('keyup', e => {
                 delay(() => {
@@ -173,7 +198,7 @@
                     if (searchQuery.length > 1 || searchQuery.length === 0) {
                         pageState.searchFilter = searchQuery;
                         $.SetQueryStringParameter('query', pageState.searchFilter);
-                        render.renderHelpRequests(pageState);
+                        render.renderData(pageState);
                     }
                 }, 500);
             });
@@ -181,19 +206,19 @@
             $('#statusFilter').on('change', function () {
                 pageState.status = this.value;
                 $.SetQueryStringParameter('status', pageState.status);
-                render.renderHelpRequests(pageState);
+                render.renderData(pageState);
             });
 
             $('#startDateFilter').on('change', function() {
                 pageState.startDate = $('#startDateFilter').val();
                 $.SetQueryStringParameter('startDate', pageState.startDate);
-                render.renderHelpRequests(pageState);
+                render.renderData(pageState);
             });
 
             $('#endDateFilter').on('change', function() {
                 pageState.endDate = $('#endDateFilter').val();
                 $.SetQueryStringParameter('endDate', pageState.endDate);
-                render.renderHelpRequests(pageState);
+                render.renderData(pageState);
             });
 
             $('.resultsPerPage').on('change', function () {
@@ -203,14 +228,14 @@
                 pageState.page = 1;
                 $.SetQueryStringParameter('page', pageState.page);
 
-                render.renderHelpRequests(pageState);
+                render.renderData(pageState);
             });
 
             $('body').on('click', 'a.page-link', function(event) {
                 event.preventDefault();
                 pageState.page = $(this).data('page');
                 $.SetQueryStringParameter('page', pageState.page);
-                render.renderHelpRequests(pageState);
+                render.renderData(pageState);
             });
         });
 
@@ -221,106 +246,5 @@
                 timer = setTimeout(callback, ms);
             };
         })();
-
-        class HelpRequestRenderer {
-            renderHelpRequests(pageState) {
-                axios.get('{{ route('ajax.help-requests') }}', {params: pageState})
-                    .then(res => {
-                        this.updateResultsCount(res.data.total);
-                        this.renderTable(res.data.data);
-                        this.renderPagination(res.data);
-                    });
-            }
-
-            emptyTable() {
-                $('#tableBody tr').remove();
-            }
-
-            updateResultsCount(count) {
-                $('#totalResults').text(count);
-
-                if (0 === count) {
-                    $('.no-results').removeClass('d-none').addClass('d-flex');
-                    $('.details').addClass('d-none');
-                } else {
-                    $('.no-results').removeClass('d-flex').addClass('d-none');
-                    $('.details').removeClass('d-none');
-                }
-            }
-
-            renderTable(responseData) {
-                this.emptyTable();
-
-                $.each(responseData, function(key, value) {
-                    let row = '<tr>\n' +
-                        '    <td><a href="/admin/help/' + value.id + '">#' + value.id + '</a></td>\n' +
-                        '    <td>' + value.patient_full_name + '</td>\n' +
-                        '    <td>' + value.caretaker_full_name + '</td>\n' +
-                        '    <td>' + value.diagnostic + '</td>\n' +
-                        '    <td>' + $.TranslateRequestStatus(value.status) + '</td>\n' +
-                        '    <td>' + moment(value.created_at).locale('ro').format('LLL') + '</td>\n' +
-                        '    <td class="text-right">\n' +
-                        '        <a href="/admin/help/' + value.id + '" class="btn btn-info btn-icon btn-sm" data-original-title="{{ __('Details') }}" title="{{ __('Details') }}">\n' +
-                        '            {{ __('See details') }}\n' +
-                        '        </a>\n' +
-                        '    </td>\n' +
-                        '</tr>';
-
-                    $('#tableBody').append(row);
-                });
-            }
-
-            renderPagination(response) {
-                $('.pagination li').remove();
-
-                if (1 === response.last_page) {
-                    return;
-                }
-
-                let morePages = '<li class="page-item disabled"><a class="page-link" href="#">...</a></li>';
-
-                let currentPage = '<li class="page-item active"><a class="page-link" data-page="' + response.current_page + '" href="#">' + response.current_page + ' <span class="sr-only">(current)</span></a></li>';
-
-                let firstPage = '';
-                if (response.current_page > 1) {
-                    firstPage = '<li class="page-item"><a class="page-link" data-page="1" href="#">1</a></li>';
-                }
-
-                let step = response.current_page
-                let counter = 0;
-
-                let previousPages = '';
-                while(step > 2 && 2 > counter) {
-                    counter++;
-                    step--;
-                    previousPages = '<li class="page-item"><a class="page-link" data-page="' + step + '" href="#">' + step + '</a></li>' + previousPages;
-                }
-
-                if (response.current_page > 4) {
-                    previousPages = morePages + previousPages;
-                }
-
-                step = response.current_page;
-                counter = 0;
-
-                let nextPages = '';
-                while(step < response.last_page - 1 && 2 > counter) {
-                    counter++;
-                    step++;
-                    nextPages += '<li class="page-item"><a class="page-link" data-page="' + step + '" href="#">' + step + '</a></li>';
-                }
-
-                if ((response.last_page - response.current_page) > 3) {
-                    nextPages += morePages;
-                }
-
-                let lastPage = '';
-                if (response.current_page < response.last_page) {
-                    lastPage = '<li class="page-item"><a class="page-link" data-page="' + response.last_page + '" href="#">' + response.last_page + '</a></li>';
-                }
-
-                $('.pagination').append(firstPage).append(previousPages).append(currentPage).append(nextPages).append(lastPage);
-            }
-        }
     </script>
 @endsection
