d7fe59be74db723e5b1d64fa4c1aafff53021ac7
wasile <wasile@gmail.com>
2020-09-01 14:20:34 +0300

    #64 added intervals table

---
+++ database/migrations/2020_09_01_100342_create_accomodations_unavailable_intervals.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccomodationsUnavailableIntervals extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accomodations_unavailable_intervals', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('accommodation_id');
+            $table->timestamp('from_date');
+            $table->timestamp('to_date');
+            $table->timestamps();
+        });
+
+        DB::statement("
+            INSERT INTO accomodations_unavailable_intervals
+                (accommodation_id, from_date, to_date, created_at)
+            SELECT id, unavailable_from_date, unavailable_to_date, NOW()
+            FROM accommodations
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accomodations_unavailable_intervals');
+    }
+}
