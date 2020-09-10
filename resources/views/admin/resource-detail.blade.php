73e5cc8bedacbf28d788269e317ddf6d9deb2815
wasile <wasile@gmail.com>
2020-09-10 13:26:00 +0300

    #139

--- resources/views/admin/resource-detail.blade.php
+++ resources/views/admin/resource-detail.blade.php
@@ -8,7 +8,7 @@
     <div class="card shadow">
         <div class="card-header bg-admin-blue py-3 d-flex justify-content-between align-content-center">
             <h6 class="font-weight-600 text-white mb-0">
-                {{ $helpResourceType->resourcetype->name }}
+                {{ $typeTranslation }}
                 @if ($helpResourceType->resourcetype->options == \App\ResourceType::OPTION_MESSAGE) ({{ $helpResourceType->helpresource->message }}) @endif
                 -
                 {{ $helpResourceType->helpresource->full_name }}
