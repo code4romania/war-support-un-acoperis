1aef8388df1fdcaa9e17bbf8074f98018a2ec92d
andreipetcu <petcumihai.andrei@gmail.com>
2020-09-13 22:46:39 +0300

    #146 Add admin profile
    #146 Move 2FA in admin layout

---
+++ resources/views/admin/reset-password.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.admin')
+
+@section('content')
+    <section class="mb-5">
+        <h6 class="page-title mb-3 font-weight-600">{{ __("Reset password") }}</h6>
+        <a href="{{ route('admin.profile') }}" class="btn btn-sm btn-outline-primary mr-3">{{ __("Back") }}</a>
+    </section>
+
+    <div class="card shadow">
+        <div class="card-header bg-admin-blue py-3 d-flex justify-content-between align-content-center">
+            <h6 class="font-weight-600 text-white mb-0">
+                {{ __("Account information") }}
+            </h6>
+        </div>
+        <div class="card-body pt-4">
+            <form action="{{ @route('admin.save-reset-password') }}" method="post">
+                @csrf
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <label for="" class="required font-weight-600">{{ __("Current password") }}:</label>
+                            <div class="pwd-container">
+                                <input type="password" name="currentPassword" id="currentPassword" class="form-control @error('currentPassword') is-invalid @enderror" placeholder="{{ __("Current password placeholder") }}">
+                                @error('currentPassword')
+                                <span class="invalid-feedback" role="alert">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <label for="" class="required font-weight-600">{{ __("New password") }}:</label>
+                            <div class="pwd-container">
+                                <input type="password" name="newPassword" id="newPassword" class="form-control @error('newPassword') is-invalid @enderror" placeholder="{{ __("New password placeholder") }}">
+                                @error('newPassword')
+                                <span class="invalid-feedback" role="alert">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-6">
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                            <label for="" class="required font-weight-600">{{ __("Retype new password") }}:</label>
+                            <div class="pwd-container">
+                                <input type="password" name="retypeNewPassword" id="retypeNewPassword" class="form-control @error('retypeNewPassword') is-invalid @enderror" placeholder="{{ __("Retype new password placeholder") }}">
+                                @error('retypeNewPassword')
+                                <span class="invalid-feedback" role="alert">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="border-top pt-4 pb-3 mt-5 clearfix">
+                    <button type="submit" id="submit-button-2" class="btn btn-secondary pull-right btn-lg px-6">
+                        <span class="btn-inner--text">{{ __("Save") }}</span>
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
+
