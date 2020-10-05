099fc5751483d83fb53fd0de257d812d4e081453
andreipetcu <petcumihai.andrei@gmail.com>
2020-10-05 22:17:15 +0300

    #163 Update partners card title bg color

--- resources/views/site/blocks/partners.blade.php
+++ resources/views/site/blocks/partners.blade.php
@@ -11,7 +11,7 @@
                         <img src="{{ $partner->image('logo', 'desktop') }}" class="mx-auto my-auto" alt="{{ $partner->title }}" style="max-height: 115px;">
                     </a>
                 </div>
-                <div class="card-footer py-2 px-4 bg-dark">
+                <div class="card-footer py-2 px-4" style="background-color: #22B0E5;">
                     <h6 class="card-title font-weight-600 m-0">
                         <a href="{{ $partner->url }}" class="text-white">{{ $partner->title }}</a>
                     </h6>
