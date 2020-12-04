83e416c7c43140853a86f26dad3f3fd9837dff97
andreipetcu <petcumihai.andrei@gmail.com>
2020-12-04 12:34:03 +0200

    #185 Add cookie bar

---
+++ resources/views/vendor/cookieConsent/dialogContents.blade.php
@@ -0,0 +1,11 @@
+<div class="js-cookie-consent cookie-consent fixed-bottom p-2 bg-primary text-white text-center">
+
+    <span class="cookie-consent__message">
+        {!! trans('cookieConsent::texts.message') !!}
+    </span>
+
+    <button class="js-cookie-consent-agree cookie-consent__agree btn btn-success">
+        {{ trans('cookieConsent::texts.agree') }}
+    </button>
+
+</div>
