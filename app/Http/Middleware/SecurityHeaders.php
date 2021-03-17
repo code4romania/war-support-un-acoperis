3d170c3b20e951e1e7a19fb3106c7a383a444a4e
andreipetcu <petcumihai.andrei@gmail.com>
2021-03-17 23:11:43 +0200

    #232 Add security headers

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -27,6 +27,13 @@ class SecurityHeaders
 
         if (method_exists($response, 'header')) {
             $response->headers->remove('x-cdn');
+            if (app()->environment('production')) {
+                $response->headers->add([
+                    'Content-Security-Policy' => "default-src fonts.googleapis.com *.amazonaws.com *.google.com impreunapentrusanatate.ro dev.impreunapentrusanatate.ro; script-src 'self' 'unsafe-inline' *.google.com www.gstatic.com; object-src 'none'; style-src 'self' 'unsafe-inline' fonts.googleapis.com; img-src 'self' data: *.amazonaws.com; frame-src 'self' *.google.com; font-src 'self' fonts.googleapis.com fonts.gstatic.com",
+                    'X-Content-Security-Policy' => "default-src fonts.googleapis.com *.amazonaws.com *.google.com impreunapentrusanatate.ro dev.impreunapentrusanatate.ro; script-src 'self' 'unsafe-inline' *.google.com www.gstatic.com; object-src 'none'; style-src 'self' 'unsafe-inline' fonts.googleapis.com; img-src 'self' data: *.amazonaws.com; frame-src 'self' *.google.com; font-src 'self' fonts.googleapis.com fonts.gstatic.com",
+                    'X-WebKit-CSP' =>  "default-src fonts.googleapis.com *.amazonaws.com *.google.com helpforhealth.local impreunapentrusanatate.ro dev.impreunapentrusanatate.ro; script-src 'self' 'unsafe-inline' *.google.com www.gstatic.com; object-src 'none'; style-src 'self' 'unsafe-inline' fonts.googleapis.com; img-src 'self' data: *.amazonaws.com; frame-src 'self' *.google.com; font-src 'self' fonts.googleapis.com fonts.gstatic.com"
+                ]);
+            }
         }
 
         return $response;
