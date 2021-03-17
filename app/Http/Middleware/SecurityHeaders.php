f015716881b49f2d2a2239eb56ac340158e468ad
andreipetcu <petcumihai.andrei@gmail.com>
2021-03-17 09:30:42 +0200

    #232 #233 Add security headers

---
+++ app/Http/Middleware/SecurityHeaders.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Response;
+
+class SecurityHeaders
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        /** @var Response $response */
+        $response = $next($request);
+
+        if (method_exists($response, 'header')) {
+            $response->header('X-Content-Type-Options', 'nosniff')
+                ->header('Referrer-Policy', 'same-origin');
+        }
+
+        return $response;
+    }
+}
