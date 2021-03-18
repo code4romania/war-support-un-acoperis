5840179bd26466f448905529fada471df858fa5e
Marius Ghitoiu <marius.ghitoiu@citizennext.ro>
2021-03-18 16:50:22 +0200

    Add empty seeder.

---
+++ database/seeds/AdminUserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+/**
+ * Class AdminUserSeeder
+ */
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        if (empty(User::all()->count())) {
+            //users to be created here.
+        }
+    }
+}
