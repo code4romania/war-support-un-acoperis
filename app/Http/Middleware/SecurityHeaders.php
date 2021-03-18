68da70aa42a7214bf916317eb901868aa357ffd0
Marius Ghitoiu <marius.ghitoiu@citizennext.ro>
2021-03-18 14:40:47 +0200

    Update allowed links.

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -10,15 +10,15 @@ class SecurityHeaders
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
         $allowedHosts = ['helpforhealth.local', 'impreunapentrusanatate.ro', 'dev.impreunapentrusanatate.ro'];
 
-        if (! in_array($request->headers->get('host'), $allowedHosts)) {
+        if (!in_array($request->headers->get('host'), $allowedHosts)) {
             abort(418);
         }
 
@@ -27,12 +27,14 @@ class SecurityHeaders
 
         if (method_exists($response, 'header')) {
             $response->headers->remove('x-cdn');
-            if (app()->environment('production')) {
-                $response->headers->add([
-                    'Content-Security-Policy' => "default-src fonts.googleapis.com *.amazonaws.com *.google.com impreunapentrusanatate.ro dev.impreunapentrusanatate.ro; script-src 'self' 'unsafe-inline' 'unsafe-eval' *.google.com www.gstatic.com; object-src 'none'; style-src 'self' 'unsafe-inline' fonts.googleapis.com; img-src 'self' data: *.amazonaws.com; frame-src 'self' *.google.com; font-src 'self' fonts.googleapis.com fonts.gstatic.com",
-                    'X-Content-Security-Policy' => "default-src fonts.googleapis.com *.amazonaws.com *.google.com impreunapentrusanatate.ro dev.impreunapentrusanatate.ro; script-src 'self' 'unsafe-inline' 'unsafe-eval' *.google.com www.gstatic.com; object-src 'none'; style-src 'self' 'unsafe-inline' fonts.googleapis.com; img-src 'self' data: *.amazonaws.com; frame-src 'self' *.google.com; font-src 'self' fonts.googleapis.com fonts.gstatic.com",
-                    'X-WebKit-CSP' =>  "default-src fonts.googleapis.com *.amazonaws.com *.google.com helpforhealth.local impreunapentrusanatate.ro dev.impreunapentrusanatate.ro; script-src 'self' 'unsafe-inline' 'unsafe-eval' *.google.com www.gstatic.com; object-src 'none'; style-src 'self' 'unsafe-inline' fonts.googleapis.com; img-src 'self' data: *.amazonaws.com; frame-src 'self' *.google.com; font-src 'self' fonts.googleapis.com fonts.gstatic.com"
-                ]);
+            if (app()->environment(['production', 'development'])) {
+                $response->headers->add(
+                    [
+                        'Content-Security-Policy' => "default-src fonts.googleapis.com *.amazonaws.com *.google.com impreunapentrusanatate.ro dev.impreunapentrusanatate.ro; script-src 'self' 'unsafe-inline' 'unsafe-eval' *.google.com www.gstatic.com polyfill.io maps.googleapis.com; object-src 'none'; style-src 'self' 'unsafe-inline' fonts.googleapis.com; img-src 'self' data: *.amazonaws.com maps.googleapis.com maps.gstatic.com; frame-src 'self' *.google.com; font-src 'self' fonts.googleapis.com fonts.gstatic.com;",
+                        'X-Content-Security-Policy' => "default-src fonts.googleapis.com *.amazonaws.com *.google.com impreunapentrusanatate.ro dev.impreunapentrusanatate.ro; script-src 'self' 'unsafe-inline' 'unsafe-eval' *.google.com www.gstatic.com polyfill.io maps.googleapis.com; object-src 'none'; style-src 'self' 'unsafe-inline' fonts.googleapis.com; img-src 'self' data: *.amazonaws.com maps.googleapis.com maps.gstatic.com; frame-src 'self' *.google.com; font-src 'self' fonts.googleapis.com fonts.gstatic.com",
+                        'X-WebKit-CSP' => "default-src fonts.googleapis.com *.amazonaws.com *.google.com helpforhealth.local impreunapentrusanatate.ro dev.impreunapentrusanatate.ro; script-src 'self' 'unsafe-inline' 'unsafe-eval' *.google.com www.gstatic.com polyfill.io maps.googleapis.com; object-src 'none'; style-src 'self' 'unsafe-inline' fonts.googleapis.com; img-src 'self' data: *.amazonaws.com maps.googleapis.com maps.gstatic.com; frame-src 'self' *.google.com; font-src 'self' fonts.googleapis.com fonts.gstatic.com"
+                    ]
+                );
             }
         }
 
