d9a8b115d6985c5b40c33da59cfd3b34f98a6927
Andrei Petcu <andrei.petcu@citizennext.ro>
2021-04-02 21:29:36 +0300

    #249 Add server side pagination for login logs

--- app/Http/Controllers/Admin/LoginLogController.php
+++ app/Http/Controllers/Admin/LoginLogController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\LoginLog;
+use Illuminate\Support\Facades\DB;
 use Yajra\DataTables\Facades\DataTables;
 
 class LoginLogController extends Controller
@@ -15,22 +16,46 @@ class LoginLogController extends Controller
 
     public function search()
     {
-        $model = LoginLog::query();
+        $model = DB::table('login_logs_view')
+            ->groupBy('email_address', 'user_id')
+            ->select(['user_id', 'email_address', 'created_at']);
 
-        $model = $model->groupBy('email_address', 'user_id')
-            ->select(['user_id', 'email_address']);
-
-
-        return DataTables::eloquent($model)
-            ->only(['id', 'email_address'])
+        return DataTables::of($model)
+//        return DataTables::eloquent($model)
+            ->only(['user_id', 'email_address'])
             ->setTransformer(function ($item) {
                 return [
-                    $item->user_id,
-                    htmlentities($item->email_address, ENT_QUOTES | ENT_HTML5, 'UTF-8'),
-                    $item->failedLoginAttempts,
-                    $item->lastLogin,
+                    'user_id' => $item->user_id,
+                    'email_address' => htmlentities($item->email_address, ENT_QUOTES | ENT_HTML5, 'UTF-8'),
+                    'failed_login_attempts' => $this->getFailedLoginAttempts($item->email_address),
+                    'last_login' => $this->getLastLogin($item->email_address)
                 ];
             })
             ->toJson();
+    }
+
+    private function getFailedLoginAttempts(string $email): int
+    {
+        $entry = DB::table('login_logs')
+            ->where('email_address', $email)
+            ->where('type', LoginLog::LOGIN_LOG_TYPE_FAIL)
+            ->groupBy(['email_address'])
+            ->select(DB::raw('COUNT(id) AS failed_attempts'))
+            ->first();
+
+        if ($entry) {
+            return $entry->failed_attempts;
+        }
+
+        return 0;
+    }
+
+    private function getLastLogin(string $email)
+    {
+        return DB::table('login_logs')
+            ->where('email_address', $email)
+            ->orderBy('created_at', 'desc')
+            ->first()
+            ->created_at;
     }
 }
