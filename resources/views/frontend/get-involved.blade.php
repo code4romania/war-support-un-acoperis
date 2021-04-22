0c50f0fc9127d87203859ac1e15bb237741f2877
Andrei Petcu <andrei.petcu@citizennext.ro>
2021-04-22 22:28:32 +0300

    #252 Remove help resources phone prefix

--- resources/views/frontend/get-involved.blade.php
+++ resources/views/frontend/get-involved.blade.php
@@ -79,7 +79,8 @@
                     <div class="row">
                         <div class="col-sm-6">
                             <div><label class="required font-weight-600" for="phone">{{ __("Phone Number") }}:</label></div>
-                            @include('partials.phone', ['controlName' => 'phone', 'controlDefault' => '', 'prefixes' => $countries, 'prefixCode' => 'RO', 'prefixValue' => ''])
+                            <input type="tel" placeholder="742000000" class="form-control @error('phone') is-invalid @enderror" name="phone" id="phone" value="{{ old('phone') }}" />
+
                         </div>
                         <div class="col-sm-6">
                             <div class="form-group">
