d3ba42e11c619145cf324b8bdf5dba4d395dd3dd
Marius Ghitoiu <marius.ghitoiu@citizennext.ro>
2021-04-27 21:48:31 +0300

    #275 change partners logo in homepage and footer.

---
+++ resources/views/frontend/social.blade.php
@@ -0,0 +1,12 @@
+<li class="mx-3">
+    <a href="https://www.facebook.com/AsocMAME" target="_blank" rel="noopener"><i class="fa fa-facebook text-white"></i></a>
+</li>
+<li class="mx-3">
+    <a href="https://www.instagram.com/asociatiamame/" target="_blank" rel="noopener"><i class="fa fa-instagram text-white"></i></a>
+</li>
+<li class="mx-3">
+    <a href="https://www.youtube.com/channel/UCqFuHLZSiMR4_BTEZveMPXg" target="_blank" rel="noopener"><i class="fa fa-youtube text-white"></i></a>
+</li>
+<li class="mx-3">
+    <a href="https://github.com/code4romania/help-for-health" target="_blank" rel="noopener"><i class="fa fa-github text-white"></i></a>
+</li>
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,8 +1,7 @@
 <!-- Footer -->
 <footer class="footer pt-0 bg-brand-blue text-white">
     <div class="container pt-4">
-        @include('site.blocks.homepage-partners')
-
+        @yield('homepage-partners')
         <ul class="d-flex justify-content-around list-unstyled align-items-center flex-column flex-sm-row pb-4 border-bottom border-bottom-dark">
             <li>
                 <a class="nav-link text-white mb-2 mb-sm-0"
@@ -50,19 +49,33 @@
                 </span>
             </li>
         </ul>
-        <ul class="d-flex justify-content-center list-unstyled align-items-center flex-sm-row mt-5 mb-3 social-buttons">
-            <li class="mx-3">
-                <a href="https://www.facebook.com/AsocMAME" target="_blank" rel="noopener"><i class="fa fa-facebook text-white"></i></a>
-            </li>
-            <li class="mx-3">
-                <a href="https://www.instagram.com/asociatiamame/" target="_blank" rel="noopener"><i class="fa fa-instagram text-white"></i></a>
-            </li>
-            <li class="mx-3">
-                <a href="https://www.youtube.com/channel/UCqFuHLZSiMR4_BTEZveMPXg" target="_blank" rel="noopener"><i class="fa fa-youtube text-white"></i></a>
-            </li>
-            <li class="mx-3">
-                <a href="https://github.com/code4romania/help-for-health" target="_blank" rel="noopener"><i class="fa fa-github text-white"></i></a>
-            </li>
-        </ul>
+        <div class="container m-auto">
+            <div class="row align-items-center pt-3">
+        @if(\Request::route()->getName() == 'home')
+                    <div class="col-md-12">
+                        <ul class="d-flex justify-content-center list-unstyled align-items-center flex-sm-row social-buttons">
+                            @include('frontend.social')
+                        </ul>
+                    </div>
+        @else
+            <div class="col-md-6 ">
+                <ul class="d-flex justify-content-left list-unstyled align-items-center flex-sm-row social-buttons">
+                    @include('frontend.social')
+                </ul>
+            </div>
+            <div class="col-md-6 text-sm-right text-left d-flex justify-content-sm-end footer-logos">
+                <a href="https://fundatia-vodafone.ro/" class="ml-0">
+                    <img src="/images/logo-fvr-white.svg" height="32" alt="Fundația Vodafone România" title="Fundația Vodafone România">
+                </a>
+                <a href="https://www.asociatiamame.ro/" class="ml-5">
+                    <img src="/images/asociatia-mame-white-logo.png" height="38" alt="Asociatia Mame" title="Asociatia Mame">
+                </a>
+                <a href="https://code4.ro/ro/" class="ml-5">
+                    <img src="/images/LogoCode4RO.png" height="30" alt="Code for Romania" title="Code for Romania">
+                </a>
+            </div>
+        @endif
+            </div>
+        </div>
     </div>
 </footer>
