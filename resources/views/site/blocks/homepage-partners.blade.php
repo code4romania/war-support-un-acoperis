6d97b637873bcf930fa28e13c8801a0d29984d9e
Marius Ghitoiu <marius.ghitoiu@citizennext.ro>
2021-05-24 18:24:26 +0300

    #284 resize logos.

--- resources/views/site/blocks/homepage-partners.blade.php
+++ resources/views/site/blocks/homepage-partners.blade.php
@@ -7,7 +7,7 @@
     <li class="mx-4">
         <a href="{{ $partner->url }}" class="nav-link text-white d-flex align-items-center mb-4 mb-sm-0" title="{{ $partner->title }}">
             {{ $partner->homepage_title }}
-            <img src="{{ $partner->image('logo', 'desktop') }}" height="@if($partner->id == 1 ) 46 @endif @if($partner->id == 2) 24 @endif @if($partner->id == 3) 36 @endif @if($partner->id == 7) 34 @endif" alt="{{ $partner->title }}" class="@if($partner->id == 1) ml-4 @else ml-5 @endif align-middle">
+            <img src="{{ $partner->image('logo', 'desktop') }}" height="@if($partner->id == 1 ) 46 @endif @if($partner->id == 2) 22 @endif @if($partner->id == 3) 36 @endif @if($partner->id == 7) 34 @endif" alt="{{ $partner->title }}" class="@if($partner->id == 1) ml-4 @else ml-5 @endif align-middle">
         </a>
     </li>
     @endforeach
