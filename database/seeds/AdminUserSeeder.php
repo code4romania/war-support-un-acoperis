22fc7b0a0a485334c048dc0639a9a4ac96b3233e
Lupu Gheorghe <gheorghelupu17@gmail.com>
2022-02-26 01:44:37 +0200

    Add User seeder

--- database/seeds/AdminUserSeeder.php
+++ database/seeds/AdminUserSeeder.php
@@ -2,6 +2,7 @@
 
 use App\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 /**
  * Class AdminUserSeeder
@@ -16,7 +17,26 @@ class AdminUserSeeder extends Seeder
     public function run()
     {
         if (empty(User::all()->count())) {
-            //users to be created here.
+            $user = User::create([
+                'name' => 'Admin',
+                'email' => 'admin@code4.ro',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => Str::random(10),
+                'country_id'=> 1,
+                'city'  => 'Iasi',
+                'approved_at' => now(),]);
+            $user->assignRole(User::ROLE_ADMINISTRATOR);
+            $user = User::create([
+                'name' => 'Host',
+                'email' => 'host@code4.ro',
+                'email_verified_at' => now(),
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'remember_token' => Str::random(10),
+                'country_id'=> 1,
+                'city'  => 'Iasi',
+                'approved_at' => now(),]);
+            $user->assignRole(User::ROLE_HOST);
         }
     }
 }
