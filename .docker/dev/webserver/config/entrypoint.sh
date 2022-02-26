5a0d98696c5d0c7095b1b83c1cffc12feaf0ecf4
cstns <cstn.serban@gmail.com>
2022-02-26 22:04:15 +0200

    update entrypoint to seed nocaptcha keys & fix composer install  if statement

--- .docker/dev/webserver/config/entrypoint.sh
+++ .docker/dev/webserver/config/entrypoint.sh
@@ -8,12 +8,18 @@ then
 fi
 
 ## install composer dependencies if the vendor dir doesn't exist
-[ -d "vendor" ] && composer install
+[ ! -d "vendor" ] && composer install
 
 ## generate app-key if it doesn't exist
 if ! grep -q APP_KEY=base64 .env
 then
   php artisan key:generate
+fi
+
+# add captcha keys if they don't exist
+if [ ! $(cat .env | grep NOCAPTCHA_SECRET | sed 's/NOCAPTCHA_SECRET=//') ]; then
+  sed -i 's/NOCAPTCHA_SECRET=/NOCAPTCHA_SECRET=6LeHnKEeAAAAAAc83Y09CX4kIE0eu92lW-29ZhsI/' .env
+  sed -i 's/NOCAPTCHA_SITEKEY=/NOCAPTCHA_SITEKEY=6LeHnKEeAAAAAOqo6kw0ksQESiPwic7KO1Ao_1tq/' .env
 fi
 
 ## Start supervisord
