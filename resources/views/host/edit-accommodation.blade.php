6d5605e49254b767e5203706adbad181a95022bb
Alex Vlad <vlad@avanticart.ro>
2022-02-28 04:34:03 +0200

    styling add new recomm form

--- resources/views/host/edit-accommodation.blade.php
+++ resources/views/host/edit-accommodation.blade.php
@@ -7,7 +7,7 @@
     </section>
 
     <div class="card shadow">
-        <div class="card-header bg-admin-blue py-3 d-flex justify-content-between align-content-center">
+        <div class="card-header bg-primary py-3 d-flex justify-content-between align-content-center">
             <h6 class="font-weight-600 text-white mb-0">
                 {{ __('Accommodation details') }}
             </h6>
