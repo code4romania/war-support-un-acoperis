b306a2e98ab09669bc506ca9b704b4051c08f5a6
cstns <cstn.serban@gmail.com>
2022-02-28 21:36:56 +0200

    add stage build step & change ports for initial stage deployment
    remove .env param
    
    fix dockerfile path for the prod git workflow
    
    add copy command to k8s init container

---
+++ .github/deploy/pull.sh
@@ -0,0 +1,8 @@
+set -e
+
+cd /home/ec2-user/war-support-un-acoperis
+git reset --hard HEAD
+git pull
+
+docker-compose -f docker-compose.stage.yml build --build-arg ENVIRONMENT=production
+docker-compose up -d
