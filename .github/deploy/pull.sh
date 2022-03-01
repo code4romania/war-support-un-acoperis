a208521c896dd03bd7b49ba122d328f4400c7aba
cstns <cstn.serban@gmail.com>
2022-03-01 14:46:06 +0200

    install assets on stage deploy

--- .github/deploy/pull.sh
+++ .github/deploy/pull.sh
@@ -4,6 +4,9 @@ cd /home/ec2-user/war-support-un-acoperis
 git reset --hard HEAD
 git pull
 
+npm i
+npm run production
+
 docker-compose -f docker-compose.stage.yml build --build-arg ENVIRONMENT=production
 docker-compose -f docker-compose.stage.yml up -d
 
