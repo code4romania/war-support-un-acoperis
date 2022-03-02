86a2f2c17665f029669cffd183147b34dd2c6c82
Andrei Ioniță <hi@andrei.io>
2022-03-02 00:03:33 +0000

    config

--- config/twill.php
+++ config/twill.php
@@ -16,7 +16,7 @@ return [
         'endpoint_type' => env('MEDIA_LIBRARY_ENDPOINT_TYPE', 's3'),
         'cascade_delete' => env('MEDIA_LIBRARY_CASCADE_DELETE', false),
         'local_path' => env('MEDIA_LIBRARY_LOCAL_PATH', 'uploads'),
-        'image_service' => env('MEDIA_LIBRARY_IMAGE_SERVICE', 'A17\Twill\Services\MediaLibrary\Local'),
+        'image_service' => env('MEDIA_LIBRARY_IMAGE_SERVICE', \A17\Twill\Services\MediaLibrary\Glide::class),
         'acl' => env('MEDIA_LIBRARY_ACL', 'private'),
         'filesize_limit' => env('MEDIA_LIBRARY_FILESIZE_LIMIT', 50),
         'allowed_extensions' => ['svg', 'jpg', 'gif', 'png', 'jpeg'],
