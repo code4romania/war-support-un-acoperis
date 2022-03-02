60aa5530e3ceb4420441b447d6fc1517759e4876
Andrei Grigoriu <andrei.grigoriu1995@gmail.com>
2022-03-02 19:06:10 +0200

    Partnership bar (#94)
    * Small adjustment on the theme colors
    
    * Small form-header fix
    
    * Add partnership bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -115,6 +115,22 @@
                 </div>
             </div>
         </nav>
+        <div class="container flex flex-wrap justify-content-end">
+            <div class="inline-flex items-center justify-content-between py-4">
+                <span>{{__("A partnership with")}}</span>
+                <div class="grid items-center">
+                    <a href="http://www.dsu.mai.gov.ro/" target="_blank" rel="noopener" class="inline-block ml-4">
+                        <img src="/images/dsu.png" class="inline-block h-5" alt="">
+                    </a>
+                </div>
+            </div>
+            <div class="inline-flex items-center justify-content-between py-4 ml-4">
+                <span><span>{{__("made by")}}</span></span>
+                <a href="https://code4.ro" target="_blank" rel="noopener" class="inline-block ml-4">
+                    <img src="/images/code4romania.svg" class="inline-block h-5" alt="">
+                </a>
+            </div>
+        </div>
 
         <main>
             @yield('content')
