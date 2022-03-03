c9e9cb1acdc4d889e2d60c7d9929755b90ca70ed
Preda Mihai <predamihai14@hotmail.com>
2022-03-04 00:42:53 +0200

    add: translations for offer help page

---
+++ resources/lang/en/content.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'page' => [
+        'offer_help' => [
+            'title' => 'Offer accommodation',
+        ],
+    ],
+];
---
+++ resources/lang/ro/content.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'page' => [
+        'offer_help' => [
+            'title' => 'Oferă cazare',
+        ],
+    ],
+];
