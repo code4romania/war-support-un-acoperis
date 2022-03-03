1295fa485d7070ddb72e2ce4ba06746c92fc577a
tibrn <barontiberiu@gmail.com>
2022-03-03 22:28:06 +0200

    patch: update text for views.frontend.success

--- resources/views/frontend/host/success.blade.php
+++ resources/views/frontend/host/success.blade.php
@@ -3,8 +3,9 @@
 @section('content')
     <section class="bg-h4h-form py-5">
         <div class="container">
-            <h1>{{ __('You have registered successfully') }}! {{ __('Please check your email address') }}.</h1>
-            <p>{{ __('Check your email address so you can setup a password and get access to your account') }}.</p>
+            <h1>{{ __('Te-ai înregistrat cu succes') }}!</h1>
+            <p>{{ __('Vei fi contactat fie telefonic, fie în persoană, de un reprezentant al autorităților pentru a verifica informația pe care ne-ai furnizat-o. Între timp ți-am trimis pe email o copie a formularului pe care l-ai completat și un link pentru a putea să îți accesezi contul creat în platformă pentru a vedea statusul ofertei tale.') }}</p>
+            <p>{{__('Îți mulțumim pentru generozitate!')}}</p>
         </div>
     </section>
 @endsection
