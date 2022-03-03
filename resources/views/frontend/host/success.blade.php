bd0b23cf4a8eb6970bf117197953cc73c4bd19ac
tibrn <barontiberiu@gmail.com>
2022-03-03 22:49:42 +0200

    patch: add ro locales

--- resources/views/frontend/host/success.blade.php
+++ resources/views/frontend/host/success.blade.php
@@ -3,9 +3,9 @@
 @section('content')
     <section class="bg-h4h-form py-5">
         <div class="container">
-            <h1>{{ __('Te-ai înregistrat cu succes') }}!</h1>
-            <p>{{ __('Vei fi contactat fie telefonic, fie în persoană, de un reprezentant al autorităților pentru a verifica informația pe care ne-ai furnizat-o. Între timp ți-am trimis pe email o copie a formularului pe care l-ai completat și un link pentru a putea să îți accesezi contul creat în platformă pentru a vedea statusul ofertei tale.') }}</p>
-            <p>{{__('Îți mulțumim pentru generozitate!')}}</p>
+            <h1>{{ __('You have successfully registered') }}!</h1>
+            <p>{{ __('You will be contacted') }}</p>
+            <p>{{__('Thank you for your generosity')}}!</p>
         </div>
     </section>
 @endsection
