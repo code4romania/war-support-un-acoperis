894cb3ea6a93dad8b37ce2df45f2c417e87eee50
Beniamin <beniamin.calota@gmail.com>
2022-03-08 09:47:30 +0200

    allow refugee user to add new help requests from profile or home page (#189)
    * allow refugee user to add new help requests from profile or home page
    
    * Apply suggestions from code review
    
    Co-authored-by: beniamin.calota <beniamin.calota@emag.ro>
    Co-authored-by: Andrei Ioniță <hi@andrei.io>

--- resources/views/refugee/help-request.blade.php
+++ resources/views/refugee/help-request.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.admin')
 
 @section('content')
+    <div class="col-md-6 d-md-flex justify-content-end">
+        <a class="btn btn-secondary m-2" href="{{ route('request-services-step3') }}">{{ __('Add help request') }}</a>
+    </div>
     <div class="card-deck accomodation-list row rows-2">
         @foreach($helpRequests as $item)
             @include('common.help-request',['item'=>$item])
