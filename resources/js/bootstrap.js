1f7194efbc094be1ad21c3bca2a1da6d6cfbef90
Utwo <mihai.legat@gmail.com>
2022-03-16 21:56:23 +0200

    Looks like without this is not working. I don't know if we need to keep bootstrap in extract then

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -6,9 +6,8 @@ window._ = require("lodash");
  * code may be modified to fit the specific needs of your application.
  */
 
-try {
-    window.$ = window.jQuery = require("jquery");
-} catch (e) {}
+window.$ = window.jQuery = require("jquery");
+require("bootstrap");
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
