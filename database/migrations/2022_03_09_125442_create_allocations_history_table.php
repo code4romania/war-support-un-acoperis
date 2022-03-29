e8bbeca5655aebc62b89b4236e4bc459d278cb1d
Andrei Ioniță <hi@andrei.io>
2022-03-29 17:05:54 +0100

    allocation history tweaks

--- database/migrations/2022_03_09_125442_create_allocations_history_table.php
+++ database/migrations/2022_03_09_125442_create_allocations_history_table.php
@@ -15,11 +15,11 @@ class CreateAllocationsHistoryTable extends Migration
     {
         Schema::create('allocations_history', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('help_request_id');
-            $table->foreignId('refugee_id');
-            $table->foreignId('accommodation_id');
-            $table->foreignId('allocation_id');
-            $table->foreignId('host_id');
+            $table->foreignId('help_request_id')->constrained('help_requests')->cascadeOnDelete();
+            $table->foreignId('refugee_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('accommodation_id')->constrained('accommodations')->cascadeOnDelete();
+            $table->foreignId('allocation_id')->nullable()->constrained('allocations')->nullOnDelete();
+            $table->foreignId('host_id')->constrained('users')->cascadeOnDelete();
             $table->unsignedInteger('number_of_guest');
             $table->timestamp('deallocated_at')->nullable();
             $table->timestamp('from')->nullable();
