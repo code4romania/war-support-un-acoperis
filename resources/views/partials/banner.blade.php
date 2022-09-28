e472861b84cfdaf9b96f791b93d9397aa2db6a39
Andrei Ioniță <hi@andrei.io>
2022-09-28 13:32:15 +0100

    fix: logo height

--- resources/views/partials/banner.blade.php
+++ resources/views/partials/banner.blade.php
@@ -1,6 +1,6 @@
 <div class="bg-gray-100">
     <div class="container flex items-center py-3">
-        <img src="/images/commitglobal.svg" class="h-6 sm:h-8 shrink-0" />
+        <img src="/images/commitglobal.svg" class="h-5" />
 
         <div class="ml-3 text-sm font-medium text-gray-700">
             <span>@lang('banner.code4')</span>
