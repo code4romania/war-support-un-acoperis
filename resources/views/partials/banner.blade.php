512d89495b0fb25312248966f754f96963b436fb
Andrei Ioniță <hi@andrei.io>
2022-09-28 13:18:22 +0100

    feat: replace banner

---
+++ resources/views/partials/banner.blade.php
@@ -0,0 +1,16 @@
+<div class="bg-gray-100">
+    <div class="container flex items-center py-3">
+        <img src="/images/commitglobal.svg" class="h-6 sm:h-8 shrink-0" />
+
+        <div class="ml-3 text-sm font-medium text-gray-700">
+            <span>@lang('banner.code4')</span>
+            <a
+                href="https://www.commitglobal.org"
+                target="_blank"
+                rel="noopener"
+                class="text-blue-600 hover:underline whitespace-nowrap">
+                @lang('Find out more here.')
+            </a>
+        </div>
+    </div>
+</div>
